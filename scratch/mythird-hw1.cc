0055d8717814832738f4a6a28229febdc2576064
manan <manan@LAPTOP-S7QN6TLR>
2025-02-17 22:30:18 -0800

    Homework

---
+++ scratch/mythird-hw1.cc
@@ -0,0 +1,213 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/ssid.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/flow-monitor-module.h"
+
+// Default Network Topology
+//
+//   Wifi 10.1.3.0
+//                 AP
+//  *    *    *    *
+//  |    |    |    |    10.1.1.0
+// n5   n6   n7   n0 -------------- n1   n2   n3   n4
+//                   point-to-point  |    |    |    |
+//                                   ================
+//                                     LAN 10.1.2.0
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("ThirdScriptExample");
+
+int
+main(int argc, char* argv[])
+{
+    bool verbose = true;
+    uint32_t nPackets = 1;
+    uint32_t nCsma = 3;
+    uint32_t nWifi = 3;
+    bool tracing = false;
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+    cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
+    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+    cmd.AddValue("nPackets", "Number of packets to echo", nPackets);
+    cmd.AddValue("tracing", "Enable pcap tracing", tracing);
+
+
+    cmd.Parse(argc, argv);
+
+    // The underlying restriction of 18 is due to the grid position
+    // allocator's configuration; the grid layout will exceed the
+    // bounding box if more than 18 nodes are provided.
+    if (nWifi > 18)
+    {
+        std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box"
+                  << std::endl;
+        return 1;
+    }
+
+    if (verbose)
+    {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    //pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    //pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install(p2pNodes);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add(p2pNodes.Get(1));
+    csmaNodes.Create(nCsma);
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
+
+    NodeContainer wifiStaNodes;
+    wifiStaNodes.Create(nWifi);
+    NodeContainer wifiApNode = p2pNodes.Get(0);
+
+    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
+    YansWifiPhyHelper phy;
+    phy.SetChannel(channel.Create());
+
+    WifiMacHelper mac;
+    Ssid ssid = Ssid("ns-3-ssid");
+
+    WifiHelper wifi;
+
+    NetDeviceContainer staDevices;
+    mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
+    staDevices = wifi.Install(phy, mac, wifiStaNodes);
+
+    NetDeviceContainer apDevices;
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
+    apDevices = wifi.Install(phy, mac, wifiApNode);
+
+    MobilityHelper mobility;
+
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
+                                  "MinX",
+                                  DoubleValue(0.0),
+                                  "MinY",
+                                  DoubleValue(0.0),
+                                  "DeltaX",
+                                  DoubleValue(5.0),
+                                  "DeltaY",
+                                  DoubleValue(10.0),
+                                  "GridWidth",
+                                  UintegerValue(3),
+                                  "LayoutType",
+                                  StringValue("RowFirst"));
+
+    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                              "Bounds",
+                              RectangleValue(Rectangle(-50, 50, -50, 50)));
+    mobility.Install(wifiStaNodes);
+    mobility.Install(csmaNodes);
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(wifiApNode);
+
+    InternetStackHelper stack;
+    stack.Install(csmaNodes);
+    stack.Install(wifiApNode);
+    stack.Install(wifiStaNodes);
+
+    Ipv4AddressHelper address;
+
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pDevices);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    //Created this
+    Ipv4InterfaceContainer wifiInterfaces;
+    
+    
+    csmaInterfaces = address.Assign(csmaDevices);
+
+    address.SetBase("10.1.3.0", "255.255.255.0");
+    //Assigned wifiinterfaces here
+    wifiInterfaces = address.Assign(staDevices);
+    address.Assign(apDevices);
+    
+    
+    UdpEchoServerHelper echoServer(9);
+
+    ApplicationContainer serverApps = echoServer.Install(wifiStaNodes.Get(2));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(60.0));
+
+    // This was CSMAInterfaces but changed to wifiinterfaces
+    UdpEchoClientHelper echoClient(wifiInterfaces.GetAddress(2), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(csmaNodes.Get(2));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(60.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+
+    Simulator::Stop(Seconds(60.0));
+
+    if (tracing)
+    {
+        phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
+        pointToPoint.EnablePcapAll("third");
+        phy.EnablePcap("third", apDevices.Get(0));
+        csma.EnablePcap("third", csmaDevices.Get(0), true);
+    }
+
+    AsciiTraceHelper ascii;
+    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("mythird.tr"));
+    pointToPoint.EnablePcapAll("mythird");
+    
+    
+    Simulator::Run();
+    monitor->CheckForLostPackets();
+  
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+    
+    for(std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); ++i ){
+       Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+       std::cout << "Flow " << i->first << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+       std::cout << "  Tx Packets: " << i->second.txPackets << "\n";
+       std::cout << "  Tx Bytes:   " << i->second.txBytes << "\n";
+       std::cout << "  Rx Bytes:   " << i->second.rxBytes << "\n";
+       std::cout << "  Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1000 / 1000  << " Mbps\n";
+    }
+
+    Ptr<MobilityModel> serverMobility = wifiStaNodes.Get(2)->GetObject<MobilityModel>();
+    std::cout << "Final Echo Server Position: " << serverMobility->GetPosition() << std::endl;
+    Ptr<MobilityModel> clientMobility = csmaNodes.Get(2)->GetObject<MobilityModel>();
+    std::cout << "Final Echo Client Position: " << clientMobility->GetPosition() << std::endl;
+    Simulator::Destroy();
+    return 0;
+}
